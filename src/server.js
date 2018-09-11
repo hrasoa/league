c815cf60b1353aaa2d65ceee9e6a9bd34d8a97ca
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-09-11 19:20:39 +0200

    update flow

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import { Capture } from 'react-loadable';
 import { getBundles } from 'react-loadable/webpack';
@@ -10,42 +12,41 @@ import stats from '../build/react-loadable.json';
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
 const server = express();
-server
-  .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .set('view engine', 'pug')
-  .set('views', './src')
-  .get('/*', (req, res) => {
-    const context = {};
-    const modules = [];
-    const markup = renderToString(
-      <Capture report={moduleName => modules.push(moduleName)}>
-        <StaticRouter context={context} location={req.url}>
-          <App />
-        </StaticRouter>
-      </Capture>,
-    );
+server.use(express.static(process.env.RAZZLE_PUBLIC_DIR || ''));
+server.disable('x-powered-by');
+server.set('view engine', 'pug');
+server.set('views', './src');
+server.get('/*', (req, res: express$Response) => {
+  const context = {};
+  const modules = [];
+  const markup = renderToString(
+    <Capture report={moduleName => modules.push(moduleName)}>
+      <StaticRouter context={context} location={req.url}>
+        <App />
+      </StaticRouter>
+    </Capture>,
+  );
 
-    if (context.url) {
-      res.redirect(context.url);
-    } else {
-      const bundles = getBundles(stats, modules);
-      const chunks = bundles.filter(bundle => bundle.file.endsWith('.js'));
-      const styles = bundles.filter(bundle => bundle.file.endsWith('.css'));
+  if (context.url) {
+    res.redirect(context.url);
+  } else {
+    const bundles = getBundles(stats, modules);
+    const chunks = bundles.filter(bundle => bundle.file.endsWith('.js'));
+    const styles = bundles.filter(bundle => bundle.file.endsWith('.css'));
 
-      res.status(200).render('index', {
-        assets,
-        chunks: chunks.map(
-          chunk => (process.env.NODE_ENV === 'production'
-            ? `/${chunk.file}`
-            : `http://${process.env.HOST}:${parseInt(process.env.PORT, 10) + 1}/${chunk.file}`
-          ),
+    res.status(200).render('index', {
+      assets,
+      chunks: chunks.map(
+        chunk => (process.env.NODE_ENV === 'production'
+          ? `/${chunk.file}`
+          : `http://${process.env.HOST || ''}:${parseInt(process.env.PORT, 10) + 1}/${chunk.file}`
         ),
-        markup,
-        process,
-        styles,
-      });
-    }
-  });
+      ),
+      markup,
+      process,
+      styles,
+    });
+  }
+});
 
 export default server;
