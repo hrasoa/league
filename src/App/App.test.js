109903bca66176164b1886d030777fc4b63854ef
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-09-13 00:40:15 +0200

    Update structure

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MemoryRouter from 'react-router-dom/MemoryRouter';
-import App from './App';
+import App from '.';
 
 describe('<App />', () => {
   test('renders without exploding', () => {
--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
-import Home from './Home';
-import Header from './Header';
-import Players from './Players';
-import Nav from './Nav';
-import Leaderboard from './LeaderBoard';
+import Home from '../Home';
+import Header from '../Header';
+import Players from '../Players';
+import Nav from '../Nav';
+import Leaderboard from '../LeaderBoard';
 import './App.scss';
 
 const App = () => (
