9473396912f6d0baa69df7bac97002235e77a86a
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-09-14 18:58:56 +0200

    Update styles

--- src/classname.js
+++ src/classname.js
@@ -1,5 +1,8 @@
 // @flow
 
-const classname = (...className: string[]): string => [...className].join(' ').trim();
+const classname = (...className: string[]): string => [...className]
+  .filter(name => typeof name === 'string')
+  .join(' ')
+  .trim();
 
 export default classname;
