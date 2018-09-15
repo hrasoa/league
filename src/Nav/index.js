286305bdb55aa5e6e0314196f8cadae0a3811cde
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-09-15 02:19:25 +0200

    Update nav

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -8,6 +8,25 @@ import Logo from '../Logo';
 import User from './User';
 import styles from './Nav.scss';
 
+const links = [
+  {
+    label: 'Players',
+    to: '/players',
+  },
+  {
+    label: 'Teams',
+    to: '/teams',
+  },
+  {
+    label: 'Leagues',
+    to: '/leagues',
+  },
+  {
+    label: 'Mercato',
+    to: '/mercato',
+  },
+];
+
 const Nav = () => (
   <nav className={styles.root}>
     <div className={styles.fixed}>
@@ -18,26 +37,13 @@ const Nav = () => (
               <Logo className={styles.logo} />
             </Link>
           </li>
-          <li className={uMarginRight}>
-            <Link to="/players">
-              Players
-            </Link>
-          </li>
-          <li className={uMarginRight}>
-            <Link to="/teams">
-              Teams
-            </Link>
-          </li>
-          <li className={uMarginRight}>
-            <Link to="/leagues">
-              Leagues
-            </Link>
-          </li>
-          <li className={uMarginRight}>
-            <Link to="/mercato">
-              Mercato
-            </Link>
-          </li>
+          {links.map((link, i) => (
+            <li key={link.to} className={uMarginRight}>
+              <Link to={`${i}-${link.to}`}>
+                {link.label}
+              </Link>
+            </li>
+          ))}
         </ul>
         <User />
       </div>
