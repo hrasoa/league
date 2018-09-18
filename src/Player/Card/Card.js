c7625ea92ea4d8fedf58f90d0edb1a3075991119
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-09-18 12:11:23 +0200

    Update player card: add gradients

--- src/Player/Card/Card.js
+++ src/Player/Card/Card.js
@@ -5,19 +5,21 @@ import team from './team.png';
 
 const Card = () => (
   <div className={styles.root}>
-    <div className={styles.wrapper}>
-      <div className={styles.outerShadow}>
-        <div className={styles.outerShape}>
-          <div className={styles.innerShape}>
-            <span className={styles.team}>
-              <img alt="" src={team} />
-            </span>
-            <img alt="" src={image} />
+    <div className={styles.inner}>
+      <div className={styles.wrapper}>
+        <div className={styles.outerShadow}>
+          <div className={styles.outerShape}>
+            <div className={styles.innerShape}>
+              <span className={styles.team}>
+                <img alt="" src={team} />
+              </span>
+              <img alt="" src={image} />
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.bio}>
-        <span className={styles.name}>aiirmad</span>
+        <div className={styles.bio}>
+          <span className={styles.name}>aiirmad</span>
+        </div>
       </div>
     </div>
   </div>
