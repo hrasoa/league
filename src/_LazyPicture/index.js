c1b33d0e5907b40ee4e9ec9d33d5c6010109e5f0
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-09-30 00:40:17 +0200

    Upate lazyPicture name, packages

--- src/_LazyPicture/index.js
+++ src/_LazyPicture/index.js
@@ -1,6 +1,6 @@
 import Loadable from 'react-loadable';
 
 export default Loadable({
-  loader: () => import(/* webpackChunkName: "lazy-picture" */'./Picture'),
+  loader: () => import(/* webpackChunkName: "lazy-picture" */'./LazyPicture'),
   loading: () => null,
 });
