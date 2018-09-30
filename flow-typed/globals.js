17255fe441c543c05dd8290cc70c7a72dec45640
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-09-30 15:55:30 +0200

    Update flow wrappers

--- flow-typed/globals.js
+++ flow-typed/globals.js
@@ -5,13 +5,3 @@ declare var module: {
     accept: (path?: string, callback?: () => void | Promise<void>) => void
   }
 }
-
-declare module CSSModule {
-  declare var exports: { [key: string]: string };
-  declare export default typeof exports;
-}
-
-declare module URLLoader {
-  declare var e: { src: string, preSrc: string };
-  declare export default typeof e;
-}
