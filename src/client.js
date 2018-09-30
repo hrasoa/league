5b89457634f7982c19b72ea704e3900e18b8ac59
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-09-30 17:14:45 +0200

    Fix fonts imports: use css instead of scss

--- src/client.js
+++ src/client.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { hydrate } from 'react-dom';
 import Loadable from 'react-loadable';
 import BrowserRouter from 'react-router-dom/BrowserRouter';
-import './_generic.lora.scss';
-import './_generic.open-sans.scss';
+import './_generic.lora.css';
+import './_generic.open-sans.css';
 import App from './App';
 
 const root = document.getElementById('root');
