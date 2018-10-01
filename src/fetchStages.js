15ea86042cab492efea5d3eb6cc8dc9108ec86d4
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-10-01 17:13:10 +0200

    Update fonts loading

--- src/fetchStages.js
+++ src/fetchStages.js
@@ -1,9 +1,19 @@
 // Use babel-repl to minify
 ((stages) => {
   const docEl = document.documentElement;
+  const sessions = getSessions(stages).filter(Boolean);
 
-  if ('fonts' in document) {
+  if (sessions.length
+    || ('serviceWorker' in navigator && navigator.serviceWorker.controller !== null && navigator.serviceWorker.controller.state === 'activated')
+  ) {
+    // do nothing
+  } else if ('fonts' in document) {
     fetchStage(stages);
+  }
+
+  function getSessions(stage) {
+    const arr = [stage.name in sessionStorage];
+    return stage.next ? [...arr, ...getSessions(stage.next)] : arr;
   }
 
   function fetchFonts(fonts) {
@@ -13,7 +23,7 @@
   function fetchStage(stage) {
     fetchFonts(stage.fonts).then(() => {
       if (stage.className) {
-        docEl.classList.add(stage.name);
+        docEl.classList.add(stage.className);
       }
       if (stage.name) {
         sessionStorage[stage.name] = true;
---
+++ src/initSwStage.js
@@ -0,0 +1,26 @@
+// Use babel-repl to minify
+((stages) => {
+  const docEl = document.documentElement;
+  const classNames = getClassNames(stages).filter(Boolean);
+
+  if (classNames.length
+    || ('serviceWorker' in navigator && navigator.serviceWorker.controller !== null && navigator.serviceWorker.controller.state === 'activated')
+  ) {
+    classNames.forEach((className) => {
+      docEl.classList.add(className);
+    });
+  }
+
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker.register('/sw.js').then((reg) => {
+  //     console.log('Service worker registered.', reg);
+  //   }).catch(() => {
+  //     console.error('Failed to load service worker');
+  //   });
+  // }
+
+  function getClassNames(stage) {
+    const arr = [(sessionStorage[stage.name] && stage.className) || null];
+    return stage.next ? [...arr, ...getClassNames(stage.next)] : arr;
+  }
+})(window.fonts);
