6982ce6f15ff23b4b43593bef9657a06ace7c43d
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-10-02 22:52:54 +0200

    Update Nav search

--- src/App/_Nav/Search.js
+++ src/App/_Nav/Search.js
@@ -1,17 +1,47 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import { IoMdSearch } from 'react-icons/io';
 import styles from './Search.scss';
 
-const Search = () => (
-  <form action="/search">
-    <span className={styles.root}>
-      <input className={styles.input} placeholder="Search..." type="search" />
-      <button className={styles.button} type="submit">
-        <IoMdSearch className={styles.icon} />
-      </button>
-    </span>
-  </form>
-);
+type State = {
+  value: string,
+}
+
+class Search extends Component<{}, State> {
+  state = {
+    value: '',
+  };
+
+  handleChange = (event: SyntheticEvent<HTMLInputElement>) => {
+    this.setState({ value: event.currentTarget.value });
+  }
+
+  handleSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
+    const { value } = this.state;
+    console.log(`A name was submitted${value}`);
+    event.preventDefault();
+  }
+
+  render() {
+    const { value } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <span className={styles.root}>
+          <input
+            name="q"
+            className={styles.input}
+            placeholder="Search..."
+            type="search"
+            value={value}
+            onChange={this.handleChange}
+          />
+          <button className={styles.button} type="submit">
+            <IoMdSearch className={styles.icon} />
+          </button>
+        </span>
+      </form>
+    );
+  }
+}
 
 export default Search;
