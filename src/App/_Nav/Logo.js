fa5ec7ea7b8d0d6f160b61d9b02dd4ca7a419790
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-10-03 01:58:19 +0200

    Update logo link

--- src/App/_Nav/Logo.js
+++ src/App/_Nav/Logo.js
@@ -1,15 +1,17 @@
 // @flow
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import withRouter from '../../_Router/withRouter';
+import type { UrlFormatter } from '../../_Router/type';
 import LogoImage from '../../_Logo';
 import styles from './Logo.scss';
 
-const Logo = () => (
+const Logo = ({ url }: { url: UrlFormatter }) => (
   <span className={styles.root}>
-    <NavLink title="Home" to="/">
+    <NavLink title="Home" to={url('home')}>
       <LogoImage className={styles.logo} />
     </NavLink>
   </span>
 );
 
-export default Logo;
+export default withRouter(Logo);
