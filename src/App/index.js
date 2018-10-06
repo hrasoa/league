a73841098299fc71ff56213851517a93ecca61c8
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-10-06 14:57:48 +0200

    Update app styles

--- src/App/index.js
+++ src/App/index.js
@@ -4,7 +4,8 @@ import { Helmet } from 'react-helmet';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import classname from 'classnames';
-import styles from './Main.scss'; // eslint-disable-line import/order
+import main from './Main.scss'; // eslint-disable-line import/order
+import header from './Header.scss'; // eslint-disable-line import/order
 import './App.scss';
 import page from '../_elements.page.scss';
 import withRouter from '../_Router/withRouter';
@@ -13,18 +14,17 @@ import Banner from '../_Ads';
 import Home from '../Home';
 import Search from '../Search';
 import Nav from '../_Nav';
-import Header from './Header';
 
 const App = ({ url }: { url: UrlFormatter }) => (
   <Fragment>
     <Helmet>
       {typeof document === 'undefined' && <html lang="en" className={classname(page.html, page.bg)} />}
     </Helmet>
-    <Header>
+    <div className={header.root}>
       <Nav />
       <Banner />
-    </Header>
-    <div className={styles.main}>
+    </div>
+    <div className={main.root}>
       <Switch>
         <Route component={Home} exact path={url('home')} />
         <Route component={Search} exact path={url('search')} />
