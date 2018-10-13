4a9ad67bed7c625e8cf1b304325ace0ab77a1b64
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-10-13 06:08:15 +0200

    Update search page

--- src/Search/All/Titles.js
+++ src/Search/All/Titles.js
@@ -6,7 +6,7 @@ import styles from './Titles.scss';
 
 const Titles = ({ children, url }: { children: Node, url: string }) => (
   <div className={styles.root}>
-    <span className={styles.titles}>{children}</span>
+    <span className={styles.title}>{children}</span>
     <a className={styles.more} href={url}>
       See more
       <IoIosArrowRoundForward className={styles.moreIcon} />
