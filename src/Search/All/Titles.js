64f4f324b1dcde33589e11f02cd7e8a047f91051
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-10-13 13:25:44 +0200

    Update search page

--- src/Search/All/Titles.js
+++ src/Search/All/Titles.js
@@ -1,11 +1,12 @@
 // @flow
 import React from 'react';
 import type { Node } from 'react';
+import spacings from 'inuitcss/utilities/_utilities.spacings.scss';
 import { IoIosArrowRoundForward } from 'react-icons/io';
 import styles from './Titles.scss';
 
 const Titles = ({ children, url }: { children: Node, url: string }) => (
-  <div className={styles.root}>
+  <div className={spacings.uMarginBottom}>
     <span className={styles.title}>{children}</span>
     <a className={styles.more} href={url}>
       See more
