048a2f4ec0b53a34200390f3dda839f56e6ed23a
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-10-23 14:34:48 +0200

    Update search all

--- src/Search/All/All.js
+++ src/Search/All/All.js
@@ -2,6 +2,8 @@
 import React, { Fragment } from 'react';
 import PlayerCard from '../../_PlayerCards/Basic';
 import TeamCard from '../../_TeamCards/Basic';
+import withRouter from '../../_Router/withRouter';
+import type { UrlFormatter, UrlSearch } from '../../_Router/type';
 import Titles from './Titles';
 import styles from './All.scss';
 
@@ -13,37 +15,40 @@ const items = [
   { id: '5' },
 ];
 
-const All = () => (
-  <Fragment>
-    <Titles url="/">
-      Players
-    </Titles>
-    <div className={styles.carousel}>
-      <ul className={styles.list}>
-        {items.map(item => (
-          <li key={`player-${item.id}`}>
-            <a className={styles.item} href="/">
-              <PlayerCard />
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-    <Titles url="/">
-      Teams
-    </Titles>
-    <div className={styles.carousel}>
-      <ul className={styles.list}>
-        {items.map(item => (
-          <li key={`team-${item.id}`}>
-            <a className={styles.item} href="/">
-              <TeamCard />
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </Fragment>
-);
+const All = ({ url, search }: { url: UrlFormatter, search: UrlSearch }) => {
+  const { q } = search;
+  return (
+    <Fragment>
+      <Titles url={url('search_players', q ? { search: { q } } : null)}>
+        Players
+      </Titles>
+      <div className={styles.carousel}>
+        <ul className={styles.list}>
+          {items.map(item => (
+            <li key={`player-${item.id}`}>
+              <a className={styles.item} href="/">
+                <PlayerCard />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Titles url={url('search_teams', q ? { search: { q } } : null)}>
+        Teams
+      </Titles>
+      <div className={styles.carousel}>
+        <ul className={styles.list}>
+          {items.map(item => (
+            <li key={`team-${item.id}`}>
+              <a className={styles.item} href="/">
+                <TeamCard />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Fragment>
+  );
+};
 
-export default All;
+export default withRouter(All);
--- src/Search/All/Titles.js
+++ src/Search/All/Titles.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import type { Node } from 'react';
+import { Link } from 'react-router-dom';
 import spacings from 'inuitcss/utilities/_utilities.spacings.scss';
 import { IoIosArrowRoundForward } from 'react-icons/io';
 import styles from './Titles.scss';
@@ -8,10 +9,10 @@ import styles from './Titles.scss';
 const Titles = ({ children, url }: { children: Node, url: string }) => (
   <div className={spacings.uMarginBottom}>
     <span className={styles.title}>{children}</span>
-    <a className={styles.more} href={url}>
+    <Link className={styles.more} to={url}>
       See more
       <IoIosArrowRoundForward className={styles.moreIcon} />
-    </a>
+    </Link>
   </div>
 );
 
