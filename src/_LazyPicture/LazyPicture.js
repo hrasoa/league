7f30b9c7752fe7733b5464c9b6d32ac933629dd0
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-11-18 02:40:25 +0100

    Update lazypicture hooks

--- src/_LazyPicture/LazyPicture.js
+++ src/_LazyPicture/LazyPicture.js
@@ -1,69 +1,54 @@
-// @flow
-import React, { Component } from 'react';
+/* eslint-disable react/prop-types */
+// @@flow
+import React, { useState } from 'react';
 import classnames from 'classnames';
 import Visible from '../_Visible';
-import type { Props } from './type';
 import defaultProps from './defaultProps';
 import styles from './LazyPicture.scss';
 
-type State = {
-  loaded: boolean,
+const LazyPicture = (props) => {
+  const [loaded, setLoaded] = useState(false);
+
+  function handleOnLoad() {
+    setLoaded(true);
+  }
+
+  const {
+    alt,
+    className,
+    styles: s,
+    image: { src, preSrc },
+  } = props;
+  return (
+    <Visible once>
+      {({ ref, visible }) => (
+        <div
+          ref={ref}
+          className={classnames(styles.root, className, loaded && styles.loaded)}
+        >
+          <div className={classnames((s && s.ratio) || styles.ratio)} />
+          <img
+            className={classnames(styles.pre, s && s.pre)}
+            alt={alt}
+            src={preSrc}
+          />
+          <picture className={styles.picture}>
+            <img
+              alt={alt}
+              className={classnames(styles.image, s && s.image)}
+              src={visible ? src : null}
+              onLoad={handleOnLoad}
+            />
+            <noscript>
+              <img alt={alt} className={className} src={src} />
+            </noscript>
+          </picture>
+        </div>
+      )}
+    </Visible>
+  );
 };
 
-class LazyPicture extends Component<Props, State> {
-  static defaultProps = defaultProps;
-
-  state = {
-    loaded: false,
-  };
-
-  shouldComponentUpdate(nextProps: Props, nextState: State) {
-    const { loaded } = this.state;
-    return loaded !== nextState.loaded;
-  }
-
-  handleOnLoad = () => {
-    this.setState({ loaded: true });
-  }
-
-  render() {
-    const { loaded } = this.state;
-    const {
-      alt,
-      className,
-      styles: s,
-      image,
-    } = this.props;
-    const { src, preSrc } = image;
-    return (
-      <Visible once>
-        {({ ref, visible }) => (
-          <div
-            ref={ref}
-            className={classnames(styles.root, className, loaded && styles.loaded)}
-          >
-            <div className={classnames((s && s.ratio) || styles.ratio)} />
-            <img
-              className={classnames(styles.pre, s && s.pre)}
-              alt={alt}
-              src={preSrc}
-            />
-            <picture className={styles.picture}>
-              <img
-                alt={alt}
-                className={classnames(styles.image, s && s.image)}
-                src={visible ? src : null}
-                onLoad={this.handleOnLoad}
-              />
-              <noscript>
-                <img alt={alt} className={className} src={src} />
-              </noscript>
-            </picture>
-          </div>
-        )}
-      </Visible>
-    );
-  }
-}
+LazyPicture.defaultProps = defaultProps;
 
 export default LazyPicture;
