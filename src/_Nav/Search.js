8eb18ed3fdf79da9de7bd3cc4f8fa4c568789bfb
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-11-22 22:13:23 +0100

    Update nav search use memo

--- src/_Nav/Search.js
+++ src/_Nav/Search.js
@@ -1,80 +1,54 @@
-// @flow
-import React, { Component } from 'react';
+/* eslint-disable react/prop-types */
+// @@flow
+import React, { useState } from 'react';
 import { IoMdSearch } from 'react-icons/io';
 import { matchPath } from 'react-router';
-import type { Location } from 'react-router-dom';
-import type { UrlPush, UrlFormatter, UrlSearch } from '../_Router/type';
 import withRouter from '../_Router/withRouter';
 import styles from './Search.scss';
 
-type Props = {
-  location: Location,
-  push: UrlPush,
-  url: UrlFormatter,
-  search: UrlSearch,
-};
+const Search = (props) => {
+  const { search } = props;
+  const [value, setValue] = useState(search.q || '');
 
-type State = {
-  value: string,
-};
-
-class Search extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    const { search } = this.props;
-    this.state = {
-      value: search.q || '',
-    };
+  function handleChange(event) {
+    setValue(event.currentTarget.value);
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    const { value } = this.state;
-    return nextState.value !== value;
-  }
-
-  handleChange = (event: SyntheticEvent<HTMLInputElement>) => {
-    this.setState({ value: event.currentTarget.value });
-  }
-
-  handleSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
+  function handleSubmit(event) {
     event.preventDefault();
-    const { value } = this.state;
     const q = value.trim();
     if (!q.length) {
       return;
     }
-    const { push, location, url } = this.props;
+    const { push, location, url } = props;
     const match = matchPath(location.pathname, { path: url('search') });
     push(match ? location.pathname : 'search', { search: { q } });
   }
 
-  render() {
-    const { value } = this.state;
-    return (
-      <form action="/search" onSubmit={this.handleSubmit}>
-        <span className={styles.root}>
-          <input
-            aria-labelledby="search-btn"
-            name="q"
-            id="search-q"
-            className={styles.input}
-            placeholder="Search..."
-            type="search"
-            value={value}
-            onChange={this.handleChange}
-          />
-          <button
-            id="search-btn"
-            aria-label="Search"
-            className={styles.button}
-            type="submit"
-          >
-            <IoMdSearch aria-hidden="true" className={styles.icon} />
-          </button>
-        </span>
-      </form>
-    );
-  }
-}
+  return (
+    <form action="/search" onSubmit={handleSubmit}>
+      <span className={styles.root}>
+        <input
+          aria-labelledby="search-btn"
+          name="q"
+          id="search-q"
+          className={styles.input}
+          placeholder="Search..."
+          type="search"
+          value={value}
+          onChange={handleChange}
+        />
+        <button
+          id="search-btn"
+          aria-label="Search"
+          className={styles.button}
+          type="submit"
+        >
+          <IoMdSearch aria-hidden="true" className={styles.icon} />
+        </button>
+      </span>
+    </form>
+  );
+};
 
 export default withRouter(Search);
