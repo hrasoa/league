8887d11eb2263b18e6b697e0b3a8610bbe86ecb5
Haja Rasoahaingo <hrasoahaingo@purch.com>
2018-11-24 03:53:39 +0100

    Add Apollojs

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,10 +1,8 @@
 import ApolloClient from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import fetch from 'node-fetch';
 
 const client = new ApolloClient({
   cache: new InMemoryCache().restore(window.INITIAL_STATE.data),
-  fetch,
   uri: 'http://localhost:4000/graphql',
 });
 
